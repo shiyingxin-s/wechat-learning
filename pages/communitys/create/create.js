ba88842c455f9e49229f6e6479ee670c8d334871
yangyang <928023158@qq.com>
2020-02-24 20:12:50 +0800

    bug

--- pages/communitys/create/create.js
+++ pages/communitys/create/create.js
@@ -26,17 +26,16 @@ Page({
 
     //文字动态
     contenttext: '',
-    //图片列表
-    imageList: []
-  },
-
-  onLoad: function() {
 
   },
-  onShow: function() {
+
+  onLoad: function () {
+
+  },
+  onShow: function () {
     let wxs = this
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -44,14 +43,14 @@ Page({
       hasUserInfo: true
     })
   },
-  previewImage: function(e) {
+  previewImage: function (e) {
     var current = '../../images/common/2.png';
     wx.previewImage({
       current: current, // 当前显示图片的http链接
       urls: this.data.imgalist // 需要预览的图片http链接列表
     })
   },
-  addImg: function() {
+  addImg: function () {
     let wxs = this
     wxs.setData({
       show: true
@@ -77,7 +76,7 @@ Page({
     })
   },
   // 相册选择
-  selectImg: function() {
+  selectImg: function () {
     let wxs = this
     wx.chooseImage({
       sizeType: ['original', 'compressed'], //可选择原图或压缩后的图片
@@ -100,7 +99,7 @@ Page({
       }
     })
   },
-  selectVideo: function() {
+  selectVideo: function () {
     wx.chooseVideo({
       sourceType: ['album', 'camera'],
       maxDuration: 60,
@@ -111,7 +110,7 @@ Page({
     })
   },
 
-  bindText: function(e) {
+  bindText: function (e) {
     console.log("e", e)
     this.setData({
       contenttext: e.detail.value
@@ -121,7 +120,7 @@ Page({
 
 
   //创建社区内容 ( 发布动态)
-  addDynamic: function() {
+  addDynamic: function () {
     let wxs = this
 
     app.httpRequest({
@@ -131,16 +130,29 @@ Page({
         userno: common.getStorageSync('userData').userNo,
         contenttext: wxs.data.contenttext
       },
-      success: function(res) {
+      success: function (res) {
         console.log("发布动态的响应", res)
-        if (res.code === 0) {
-          wxs.upload(res.dynamicId)
+        if (res.code == 0) {
+          console.log("wxs.data.imgalist", wxs.data.imgalist)
+          if (wxs.data.imgalist.length == 0) {
+            console.log("没有上传图片")
+            // wx.navigateTo({
+            //   url: '../community',
+            // })
+            common.showToast("发表成功")
+            wx.navigateBack()
+          } else {
+            console.log("上传了图片")
+
+            wxs.upload(res.dynamicId)
+          }
+
 
         } else {
           common.showToast(res.msg, 3000)
         }
       },
-      fail: function(res) {
+      fail: function (res) {
 
         common.showToast(res.msg, 3000)
       },
@@ -151,26 +163,33 @@ Page({
   },
 
   //上传图片的接口
-  upload: function(dynamicId) {
+  upload: function (dynamicId) {
     let wxs = this
-    wx.uploadFile({
-      url: app.globalServiceUrl.serviceUrl + '/xbg-api/api/community/upload', //根据具体后端程序IP修改
-      filePath: wxs.data.imgalist[0],
-      name: 'file',
-      formData: {
-        'token': common.getStorageSync('userData').token, //身份验证
-        'userId': common.getStorageSync('userData').userNo, //发表动态的用户ID
-        'dynamicId': dynamicId //发表动态之后返回的dynamicId
-      },
-      success: function(res) {
-        //do something
-        console.log("上传图片的响应", res)
-      }
-    })
+    for (let i in wxs.data.imgalist) {
+      wx.uploadFile({
+        url: app.globalServiceUrl.serviceUrl + '/xbg-api/api/community/upload', //根据具体后端程序IP修改
+        filePath: wxs.data.imgalist[i],
+        name: 'file',
+        formData: {
+          'token': common.getStorageSync('userData').token, //身份验证
+          'userId': common.getStorageSync('userData').userNo, //发表动态的用户ID
+          'dynamicId': dynamicId //发表动态之后返回的dynamicId
+        },
+        success: function (res) {
+          //do something
+          console.log("上传图片的响应", res)
+          common.showToast("发表成功")
+          // wx.redirectTo({
+          //   url: '../community',
+          // })
+          wx.navigateBack()
+        }
+      })
+    }
   },
 
   //发布动态
-  send: function() {
+  send: function () {
     if (this.data.contenttext) {
       this.addDynamic()
     } else {
